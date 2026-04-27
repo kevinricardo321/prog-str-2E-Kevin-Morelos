8a0c119d4ccc8aedef5df3f3bd24dfffa5e75f2f
Kevin <20253ds131@utez.edu.mx>
2026-04-27 02:16:17 -0600

    actualizacion de mensaje

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,10 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("mensaje normalgit");
+
+
+        System.out.println("mensaje nuevo");
+
 
     }
 
